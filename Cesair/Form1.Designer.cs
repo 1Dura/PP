90a81c9cc15294db9f389a5de94acda94d921ecf
1Dura <BeSTaR_X@mail.ru>
2025-03-12 12:14:09 +0300

    +Cesair

---
+++ Cesair/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Cesair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input_Textbox = new System.Windows.Forms.TextBox();
+            this.Code_Button = new System.Windows.Forms.Button();
+            this.Decode_Button = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.скачатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Input_Textbox
+            // 
+            this.Input_Textbox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.Input_Textbox.Font = new System.Drawing.Font("Segoe Print", 11F, System.Drawing.FontStyle.Bold);
+            this.Input_Textbox.ForeColor = System.Drawing.Color.Gold;
+            this.Input_Textbox.Location = new System.Drawing.Point(335, 184);
+            this.Input_Textbox.Multiline = true;
+            this.Input_Textbox.Name = "Input_Textbox";
+            this.Input_Textbox.Size = new System.Drawing.Size(408, 190);
+            this.Input_Textbox.TabIndex = 0;
+            this.Input_Textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Code_Button
+            // 
+            this.Code_Button.BackColor = System.Drawing.Color.Transparent;
+            this.Code_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Code_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Code_Button.FlatAppearance.BorderSize = 0;
+            this.Code_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Code_Button.Font = new System.Drawing.Font("Segoe Print", 16F, System.Drawing.FontStyle.Bold);
+            this.Code_Button.ForeColor = System.Drawing.Color.Yellow;
+            this.Code_Button.Location = new System.Drawing.Point(26, 185);
+            this.Code_Button.Name = "Code_Button";
+            this.Code_Button.Size = new System.Drawing.Size(281, 57);
+            this.Code_Button.TabIndex = 4;
+            this.Code_Button.Text = "Кодировать";
+            this.Code_Button.UseVisualStyleBackColor = false;
+            this.Code_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Decode_Button
+            // 
+            this.Decode_Button.BackColor = System.Drawing.Color.Transparent;
+            this.Decode_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Decode_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Decode_Button.FlatAppearance.BorderSize = 0;
+            this.Decode_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Decode_Button.Font = new System.Drawing.Font("Segoe Print", 16F, System.Drawing.FontStyle.Bold);
+            this.Decode_Button.ForeColor = System.Drawing.Color.Yellow;
+            this.Decode_Button.Location = new System.Drawing.Point(12, 248);
+            this.Decode_Button.Name = "Decode_Button";
+            this.Decode_Button.Size = new System.Drawing.Size(297, 57);
+            this.Decode_Button.TabIndex = 5;
+            this.Decode_Button.Text = "Декодировать";
+            this.Decode_Button.UseVisualStyleBackColor = false;
+            this.Decode_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.numericUpDown1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe Print", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Yellow;
+            this.numericUpDown1.Location = new System.Drawing.Point(223, 310);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(84, 64);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(719, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 47);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 16F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.Location = new System.Drawing.Point(57, 311);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 57);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Ключ ->";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe Print", 16F, System.Drawing.FontStyle.Bold);
+            this.textBox2.ForeColor = System.Drawing.Color.Gold;
+            this.textBox2.Location = new System.Drawing.Point(223, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(274, 57);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "Шифр Цезаря";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(764, 38);
+            this.menuStrip1.TabIndex = 11;
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.открытьToolStripMenuItem1});
+            this.toolStripMenuItem2.ForeColor = System.Drawing.Color.Red;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(90, 34);
+            this.toolStripMenuItem2.Text = "Файл";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.сохранитьToolStripMenuItem.ForeColor = System.Drawing.Color.Yellow;
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(270, 38);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            this.открытьToolStripMenuItem1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.открытьToolStripMenuItem1.ForeColor = System.Drawing.Color.Yellow;
+            this.открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            this.открытьToolStripMenuItem1.Size = new System.Drawing.Size(270, 38);
+            this.открытьToolStripMenuItem1.Text = "Открыть";
+            this.открытьToolStripMenuItem1.Click += new System.EventHandler(this.открытьToolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.скачатьToolStripMenuItem,
+            this.открытьToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(187, 32);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // скачатьToolStripMenuItem
+            // 
+            this.скачатьToolStripMenuItem.Name = "скачатьToolStripMenuItem";
+            this.скачатьToolStripMenuItem.Size = new System.Drawing.Size(102, 34);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(102, 34);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(764, 398);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Decode_Button);
+            this.Controls.Add(this.Code_Button);
+            this.Controls.Add(this.Input_Textbox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_Textbox;
+        private System.Windows.Forms.Button Code_Button;
+        private System.Windows.Forms.Button Decode_Button;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem скачатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem1;
+    }
+}
+
---
+++ Cesair/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace Cesair
+{
+    public partial class Form1 : Form
+    {
+        Cesair Me = new Cesair();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Input_Textbox.Text = Me.Codeс(Input_Textbox.Text, -(int)numericUpDown1.Value);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Input_Textbox.Text = Me.Codeс(Input_Textbox.Text, (int)numericUpDown1.Value);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog.FileName, Input_Textbox.Text);
+            }
+        }
+
+        private void открытьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Input_Textbox.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Clent
+    {
+        string le;
+
+        public Clent(string m)
+        {
+            le = m;
+        }
+
+        public string Repl(string m, int key) //замена символа на другой со смещением
+        {
+            int pos = le.IndexOf(m);
+            if (pos == -1) return ""; // если в лентах нет такого символа
+            pos = (pos + key) % le.Length; //если смещение больше одного круга
+            if (pos < 0) pos += le.Length;
+            return le.Substring(pos, 1);
+        }
+    }
+    class Cesair : System.Collections.Generic.List<Clent>
+    {
+        public Cesair()
+        { 
+            this.Add(new Clent("abcdefghijklmnopqrstuvwxyz"));
+            this.Add(new Clent("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            this.Add(new Clent("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"));
+            this.Add(new Clent("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"));
+            this.Add(new Clent("0123456789"));
+        }
+
+        public string Codeс(string m, int key) //код и декод в зависимости от знака ключа
+        {
+            string res = "", tmp = "";
+            for (int i = 0; i < m.Length; i++)
+            {
+                foreach (Clent v in this)
+                {
+                    tmp = v.Repl(m.Substring(i, 1), key);
+                    if (tmp != "") //нужная лента найдена
+                    {
+                        res += tmp;
+                        break;
+                    }
+                }
+                if (tmp == "") res += m.Substring(i, 1); //если нет ленты для символа - то он не меняется
+            }
+            return res;
+        }
+    }
+}
